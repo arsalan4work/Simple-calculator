340c23db19922157676a62b1706cc45a6c406c6d
arsalan4work <you@example.com>
2024-03-20 00:17:51 +0500

    some changes

--- calculator.ts
+++ calculator.ts
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer"
 const answer = await inquirer.prompt([
-    {message: "Enter first number", type: "number", name: "firstNumber"},
-    {message: "Enter second number", type: "number", name: "secondNumber"},
+    {message: "Enter your first number", type: "number", name: "firstNumber"},
+    {message: "Enter your second number", type: "number", name: "secondNumber"},
     {
         message: "Select one of the operators to perform operation",
         type: "list",
@@ -23,3 +23,5 @@ if (answer.operators === "Addition") {
 } else {
     console.log("Please select the operator");
 }
+
+console.log("The END!")
